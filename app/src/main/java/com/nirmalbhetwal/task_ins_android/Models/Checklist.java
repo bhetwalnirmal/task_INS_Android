3d38fa1ea89d6b8dbcd44414c65d1c558520f15d
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-06-16 18:07:26 -0400

    created model class

---
+++ app/src/main/java/com/nirmalbhetwal/task_ins_android/Models/Checklist.java
@@ -0,0 +1,9 @@
+package com.nirmalbhetwal.task_ins_android.Models;
+
+import java.io.Serializable;
+
+public class Checklist implements Serializable {
+    private String name;
+    private boolean isCompleted;
+    private boolean completedDate;
+}
