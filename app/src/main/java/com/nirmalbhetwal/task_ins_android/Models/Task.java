641f75e71516b9bbe23fa9db6756ff851361199e
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-06-17 02:22:46 -0400

    loaded images from gallery

--- app/src/main/java/com/nirmalbhetwal/task_ins_android/Models/Task.java
+++ app/src/main/java/com/nirmalbhetwal/task_ins_android/Models/Task.java
@@ -1,10 +1,13 @@
 package com.nirmalbhetwal.task_ins_android.Models;
 
 import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Task implements Serializable {
     private String name;
     private ArrayList<Checklist> checklists;
+    private ArrayList<String> images;
+    private String audioName;
 }
