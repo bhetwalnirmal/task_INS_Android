df5d0727a8dea3627f825c3aca03da516da5d96b
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-06-17 01:31:46 -0400

    created layout for create task

--- app/src/main/java/com/nirmalbhetwal/task_ins_android/Models/Task.java
+++ app/src/main/java/com/nirmalbhetwal/task_ins_android/Models/Task.java
@@ -1,9 +1,10 @@
 package com.nirmalbhetwal.task_ins_android.Models;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Task implements Serializable {
     private String name;
-    private Checklist[] checklists;
+    private ArrayList<Checklist> checklists;
 }
